1df29cf0ec36e004aacc4f7163a67a8ed899c4d1
Einere <kjwsx23@gmail.com>
2020-02-23 19:54:50 +0900

    Fix: add basic component & context
    간단한 입력창과 컨텍스트를 추가함.
    
    Signed-off-by: Einere <kjwsx23@gmail.com>

--- src/component/TodoInput/TodoInput.tsx
+++ src/component/TodoInput/TodoInput.tsx
@@ -1,8 +1,24 @@
 import * as React from 'react';
-import {FunctionComponent} from 'react';
+import {FunctionComponent, useCallback, useContext, useRef} from 'react';
+import {TodoInfoContext, TodoInfoContextType} from "../TodoInfoContext/TodoInfoContext";
 
 export const TodoInput: FunctionComponent = function () {
+    const todoInfoContext: TodoInfoContextType = useContext(TodoInfoContext);
+
+    const todoInputRef = useRef<HTMLInputElement>(null);
+
+    const addTodoItem = useCallback((e) => {
+        e.preventDefault();
+        if (todoInputRef && todoInputRef.current) {
+            console.log(todoInputRef.current.value);
+        }
+    }, [todoInputRef]);
+
     return (
-        <p>todo input</p>
+        <>
+            <label htmlFor={"TodoTitle"}>Todo Title</label>
+            <input type={"string"} name={"TodoTitle"} ref={todoInputRef}/>
+            <button type={"button"} onClick={addTodoItem}>Add</button>
+        </>
     );
 };
