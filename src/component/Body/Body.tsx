4518c80db10c0d372ac80979005b4d1bbfae9ca4
Einere <kjwsx23@gmail.com>
2020-02-23 19:55:55 +0900

    Fix: add basic component & context to Body
    `Body`컴포넌트에 `TodoInfoContext`와 `TodoInput`, `TodoList`컴포넌트를 추가함.
    
    Signed-off-by: Einere <kjwsx23@gmail.com>

--- src/component/Body/Body.tsx
+++ src/component/Body/Body.tsx
@@ -1,9 +1,21 @@
 import * as React from 'react';
 import {FunctionComponent} from 'react';
 import BodyStyle from './Body.style';
+import {TodoInput} from '../TodoInput/TodoInput';
+import {TodoList} from "../TodoList/TodoList";
+import {TodoInfoContext, TodoInfoContextType} from "../TodoInfoContext/TodoInfoContext";
+
+
+const todoInfoContext: TodoInfoContextType = {
+    todoInfoList: []
+};
 
 export const Body: FunctionComponent = function () {
     return (
-        <BodyStyle>body</BodyStyle>
+        <BodyStyle>
+            <TodoInfoContext.Provider value={todoInfoContext}/>
+            <TodoInput/>
+            <TodoList/>
+        </BodyStyle>
     );
 };
