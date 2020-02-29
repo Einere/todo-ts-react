3b74f455fc7f352ed01b6b8c9a0e2120ab19ef2c
Einere <kjwsx23@gmail.com>
2020-02-29 17:37:40 +0900

    style: add style to TodoInput
    `TodoInput`컴포넌트에 스타일을 추가함.
    
    Signed-off-by: Einere <kjwsx23@gmail.com>

---
+++ src/component/TodoInput/TodoInput.style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const TodoInputContainerStyle = styled.section`
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+`;
+
+export const TodoInputFieldStyle = styled.div`
+  margin: 0.5rem 0;
+`;
