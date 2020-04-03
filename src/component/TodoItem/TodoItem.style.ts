a22733f4235edc1710b1339d881844063d96f866
Einere <kjwsx23@gmail.com>
2020-04-03 16:06:54 +0900

    style: toggle detail or not
    할일 아이템을 클릭하면 자세히기 보기 / 간단히 보기 전환할 수 있도록 스타일을 수정함.
    
    Signed-off-by: Einere <kjwsx23@gmail.com>

--- src/component/TodoItem/TodoItem.style.ts
+++ src/component/TodoItem/TodoItem.style.ts
@@ -10,8 +10,13 @@ export default styled.article<TodoItemStyleProps>`
   border: 2px solid ${props => props.done ? '#77dd77' : props.expired ? '#ff6961' : '#696969'} ;
   border-radius: 10px;
   margin: 0.5rem 0;
+  transition: all 1s ease;
   
   @media only screen and (max-width: 768px) {
     width: 80%;
+  }
+  
+  & p.invisible, & span.invisible {
+    display: none;
   }
 `;
--- src/component/TodoItem/TodoItem.tsx
+++ src/component/TodoItem/TodoItem.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {FunctionComponent, useCallback, useMemo} from 'react';
+import {FunctionComponent, useCallback, useMemo, useState} from 'react';
 import {Todo} from "../todoTypes";
 import TodoItemStyle from './TodoItem.style';
 
@@ -12,20 +12,27 @@ export const TodoItem: FunctionComponent<Todo.TodoItemProp> = function ({todoInf
         setTodoInfos(newTodoInfos);
     }, [todoInfos, todoInfo, setTodoInfos]);
 
+    const [isDetail, setIsDetail] = useState<boolean>(false);
+    const toggleIsDetail = useCallback((e) => {
+        if (e.target.tagName !== 'BUTTON') setIsDetail(!isDetail);
+    }, [isDetail]);
+
     const expired = useMemo<boolean>(() => {
-        return todoInfo.done ? false : todoInfo.dueTime.getTime() < Date.now();
-    }, [todoInfo, todoInfo.done]);
+        return todoInfo.dueTime.getTime() < Date.now();
+    }, [todoInfo.dueTime]);
 
     const onHandleDelete = function () {
         deleteTodoItem(todoInfo.id);
     };
 
     return (
-        <TodoItemStyle expired={expired} done={todoInfo.done}>
+        <TodoItemStyle expired={expired} done={todoInfo.done} onClick={toggleIsDetail}>
             <h3>{todoInfo.title}</h3>
-            <p>{todoInfo.content}</p>
-            <p>{todoInfo.done ? 'O' : 'X'}</p>
-            <p>{todoInfo.createTime.toLocaleString()}/{todoInfo.dueTime.toLocaleString()}</p>
+            <p className={isDetail ? '' : 'invisible'}>{todoInfo.content}</p>
+            <p>
+                <span className={isDetail ? '' : 'invisible'}>{todoInfo.createTime.toLocaleString()} / </span>
+                <span>{todoInfo.dueTime.toLocaleString()}</span>
+            </p>
             <button onClick={toggleDone}>done</button>
             <button onClick={onHandleDelete}>delete</button>
         </TodoItemStyle>
