1ca4f2e3753721b66363d24114ae1797ed5c46d9
Einere <kjwsx23@gmail.com>
2020-05-10 17:08:36 +0900

    feat: implement create & load database
    DBContext.ts와 App.tsx에서, Loki.js를 이용한 DB를 생성하거나 localStorage에서 가져오는 기능을 구현함.
    
    Signed-off-by: Einere <kjwsx23@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import * as React from 'react';
 import './App.css';
 import Home from "./page/Home/Home";
+import {db, dbContext, todoCollectionName} from "./context/DBContext";
+import {Todo} from './component/todoTypes';
 
 const App = () => {
+    db.loadDatabase({});
+    const todoDB = db.getCollection(todoCollectionName);
+    if (!todoDB) db.addCollection<Todo.TodoInfoType>(todoCollectionName);
+
     return (
         <div className="App">
-            <Home/>
+            <dbContext.Provider value={db}>
+                <Home/>
+            </dbContext.Provider>
         </div>
     );
 };
---
+++ src/context/DBContext.ts
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as Loki from 'lokijs';
+
+export const db = new Loki('TodoDB', {
+    env: "BROWSER"
+});
+
+export const dbContext = React.createContext<Loki>(db);
+
+export const todoCollectionName = 'Todo';
