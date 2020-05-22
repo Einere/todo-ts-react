bda1d4fcfe6debd352daeabeb5d6466668ad78d5
Einere <kjwsx23@gmail.com>
2020-05-22 17:29:48 +0900

    refactor: add TodoInputForm component
    `TodoInput`과 `TodoUpdate`에서 공통적으로 사용되는 요소를 `TodoInputForm`컴포넌트로 분리함.
    
    Signed-off-by: Einere <kjwsx23@gmail.com>

---
+++ src/component/TodoInput/TodoInputForm.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import {FunctionComponent} from 'react';
+import {Form} from "react-bootstrap";
+import DatePicker from "react-datepicker";
+import {Todo} from "custom-types";
+
+export const TodoInputForm: FunctionComponent<Todo.TodoInputFormProp> = function (props) {
+    const {
+        children,
+        todoTitle,
+        todoContent,
+        todoDueTime,
+        setTodoDueTime,
+        isTitleValid,
+        isContentValid,
+        onFormSubmit,
+        onTitleChange,
+        onContentChange,
+    } = props;
+
+    return (
+        <Form onSubmit={onFormSubmit}>
+            <Form.Group controlId="todo-title">
+                <Form.Label>Title</Form.Label>
+                <Form.Control type="text" placeholder="Enter title" className={isTitleValid ? '' : 'invalid'}
+                              onChange={onTitleChange} value={todoTitle}/>
+            </Form.Group>
+            <Form.Group controlId="todo-content">
+                <Form.Label>Content</Form.Label>
+                <Form.Control as="textarea" placeholder="Enter content" className={isContentValid ? '' : 'invalid'}
+                              onChange={onContentChange}
+                              value={todoContent}/>
+            </Form.Group>
+            <Form.Group>
+                <DatePicker
+                    className="form-control data-picker-input"
+                    selected={todoDueTime}
+                    onChange={date => setTodoDueTime(date!)}
+                    showTimeInput
+                    disabledKeyboardNavigation
+                    dateFormat="yyyy MM dd hh:mm aa"
+                    shouldCloseOnSelect={false}
+                />
+            </Form.Group>
+            <Form.Group>
+                {children}
+            </Form.Group>
+        </Form>
+    );
+};
